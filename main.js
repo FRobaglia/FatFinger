7a19c06d32e000e08a0ad6b686e73cbbafbedc15
unknown <felix.robaglia@gmail.com>
2019-02-06 03:11:09 +0100

    v1.0

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
-let input = document.querySelector(".game");
-let nextWordBox = document.querySelector(".next-word");
-let timeBox = document.querySelector(".time");
-let currentWordBox = document.querySelector(".current-word");
-let darkLightButton = document.querySelector(".light-dark");
-let scoreBox = document.querySelector(".score");
-let bestScoreBox = document.querySelector(".best-score");
-let body = document.querySelector("body");
+let input = document.querySelector('.game');
+let nextWordBox = document.querySelector('.next-word');
+let timeBox = document.querySelector('.time');
+let currentWordBox = document.querySelector('.current-word');
+let darkLightButton = document.querySelector('.light-dark');
+let scoreBox = document.querySelector('.score');
+let bestScoreBox = document.querySelector('.best-score');
+let body = document.querySelector('body');
 
 let currentWord;
 let nextWord;
 
 let timer = false;
 let bestScore = 0;
-
 let score;
 let time;
 
 let names = [
-  "hetic",
-  "chrome",
-  "javascript",
-  "front-end",
-  "framework",
-  "canvas",
-  "html",
-  "css",
-  "library",
-  "terminal",
-  "bash",
-  "figma",
-  "sketch",
-  "npm",
-  "sass",
-  "internet",
-  "class",
-  "semantic",
-  "full-stack",
-  "design",
-  "firefox",
-  "cookies",
-  "parcel",
-  "flex",
-  "display",
-  "header",
-  "php",
-  "sql",
-  "back-end",
-  "indentation",
-  "codepen",
-  "database",
-  "function",
-  "responsive",
-  "svg",
-  "variable",
-  "loop",
-  "IE11",
-  "markdown",
-  "github",
-  "push",
-  "pull",
-  "commit",
-  "defer"
+  'hetic',
+  'chrome',
+  'javascript',
+  'front-end',
+  'framework',
+  'canvas',
+  'html',
+  'css',
+  'library',
+  'terminal',
+  'bash',
+  'figma',
+  'sketch',
+  'npm',
+  'sass',
+  'internet',
+  'class',
+  'semantic',
+  'full-stack',
+  'design',
+  'firefox',
+  'cookies',
+  'parcel',
+  'flex',
+  'display',
+  'header',
+  'php',
+  'sql',
+  'back-end',
+  'indentation',
+  'codepen',
+  'database',
+  'function',
+  'responsive',
+  'svg',
+  'variable',
+  'loop',
+  'IE11',
+  'markdown',
+  'github',
+  'push',
+  'pull',
+  'commit',
+  'defer'
 ];
 
-darkLightButton.addEventListener("click", function() {
-  body.classList.toggle("dark");
-  darkLightButton.classList.toggle("dark");
-  input.classList.toggle("dark");
-  if (input.classList.contains("dark")) {
-    input.style.border = "3px solid whitesmoke";
+darkLightButton.addEventListener('click', function() {
+  body.classList.toggle('dark');
+  darkLightButton.classList.toggle('dark');
+  input.classList.toggle('dark');
+  if (input.classList.contains('dark')) {
+    input.style.border = '3px solid whitesmoke';
   } else {
-    input.style.border = "3px solid #1d2935";
+    input.style.border = '3px solid #1d2935';
   }
 });
 
 function init() {
   input.value = null;
+  input.placeholder = 'type to start';
   time = 30;
   score = 0;
   timeBox.innerHTML = time;
@@ -85,6 +85,7 @@ function init() {
 }
 
 function startTimer() {
+  input.placeholder = '';
   timer = true;
   time--;
   timeBox.innerHTML = time;
@@ -98,17 +99,17 @@ function startTimer() {
 function gameOver() {
   timer = false;
   if (score > bestScore) {
-    bestScore = score;
+    bestScore = score
     bestScoreBox.innerHTML = bestScore;
   }
   init();
 }
 
 function newWord() {
-  if (input.classList.contains("dark")) {
-    input.style.border = "3px solid whitesmoke";
+  if (input.classList.contains('dark')) {
+    input.style.border = '3px solid whitesmoke';
   } else {
-    input.style.border = "3px solid #1d2935";
+    input.style.border = '3px solid #1d2935';
   }
   if (!nextWord) {
     currentWord = names[Math.floor(Math.random() * names.length)];
@@ -121,14 +122,14 @@ function newWord() {
   nextWordBox.innerHTML = nextWord;
 }
 
-input.addEventListener("keyup", function(event) {
+input.addEventListener('keyup', function(event) {
   if (!timer) {
     startTimer();
   }
-  if (input.value === "") {
+  if (input.value === '') {
     setNormalBorder();
   } else if (
-    input.value === currentWord + " " ||
+    input.value === currentWord + ' ' ||
     (input.value === currentWord && event.keyCode === 13)
   ) {
     goodAnswer();
@@ -148,18 +149,18 @@ function printScore() {
 }
 
 function setNormalBorder() {
-  if (input.classList.contains("dark")) {
-    input.style.border = "3px solid whitesmoke";
+  if (input.classList.contains('dark')) {
+    input.style.border = '3px solid whitesmoke';
   } else {
-    input.style.border = "3px solid #1d2935";
+    input.style.border = '3px solid #1d2935';
   }
 }
 
 function setGreenBorder() {
-  input.style.border = "3px solid #2ecc71";
+  input.style.border = '3px solid #2ecc71';
 }
 function setRedBorder() {
-  input.style.border = "3px solid #e74c3c";
+  input.style.border = '3px solid #e74c3c';
 }
 
 function goodAnswer() {
